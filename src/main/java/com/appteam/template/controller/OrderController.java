985a069d7eb99e10b6281533943d5e9e19327200
artsidorenko <art.sidorenko2@gmail.com>
2023-03-21 14:08:25 +0300

    minor fixes

--- src/main/java/com/appteam/template/controller/OrderController.java
+++ src/main/java/com/appteam/template/controller/OrderController.java
@@ -3,7 +3,13 @@ package com.appteam.template.controller;
 import com.appteam.template.data.Order;
 import com.appteam.template.dto.OrderData;
 import com.appteam.template.service.DefaultOrderService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 import javax.annotation.Resource;
