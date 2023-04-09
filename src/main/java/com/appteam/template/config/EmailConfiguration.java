2b64707fc26b0e2edc0a3b571d16914504117cfd
artsidorenko <art.sidorenko2@gmail.com>
2023-04-09 23:11:05 +0300

    basic email with mocks

--- src/main/java/com/appteam/template/config/EmailConfiguration.java
+++ src/main/java/com/appteam/template/config/EmailConfiguration.java
@@ -1,5 +1,6 @@
 package com.appteam.template.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
@@ -9,6 +10,11 @@ import java.util.Properties;
 
 @Component
 public class EmailConfiguration {
+
+    @Value("${spring.mail.username}")
+    private String mailUsername;
+    @Value("${spring.mail.password}")
+    private String mailPassword;
     @Bean
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
@@ -16,8 +22,8 @@ public class EmailConfiguration {
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
 
-        mailSender.setUsername("");
-        mailSender.setPassword("");
+        mailSender.setUsername(mailUsername);
+        mailSender.setPassword(mailPassword);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
--- src/main/java/com/appteam/template/service/OrderStatusNotificationService.java
+++ src/main/java/com/appteam/template/service/OrderStatusNotificationService.java
@@ -2,6 +2,7 @@ package com.appteam.template.service;
 
 import com.appteam.template.dto.OrderData;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -14,14 +15,16 @@ import java.util.List;
 @Service("emailService")
 @EnableScheduling
 public class OrderStatusNotificationService implements EmailService {
+
     @Autowired
     private JavaMailSender javaMailSender;
 
+    private static final String NOREPLY_TO = "noreply@test.com";
     @Resource(name = "orderService")
     private OrderService orderService;
     public void sendSimpleMessage(String to, String subject, String text) {
         SimpleMailMessage msg = new SimpleMailMessage();
-        msg.setFrom("detiDonbassa");
+        msg.setReplyTo(NOREPLY_TO);
         msg.setTo(to);
         msg.setSubject(subject);
         msg.setText(text);
@@ -31,15 +34,18 @@ public class OrderStatusNotificationService implements EmailService {
     public void sendOrderStatuses(String user, String status) {
         List<OrderData> orders = orderService.getUserOrdersByStatus(user, status);
         StringBuilder textBuilder = new StringBuilder();
-        orders.forEach(order -> textBuilder.append(order.getId() + '\n'));
-        sendSimpleMessage(user, "Orders with status: " + status, textBuilder.toString());
+        if (orders != null) {
+            orders.forEach(order -> textBuilder.append(order.getId()).append("\n"));
+        }
+        sendSimpleMessage(user, "Orders with status: " + status + "\n", textBuilder.toString());
     }
 
-    @Scheduled(fixedRate = 10000)
+    @Value("${test.receiver}")
+    private String testUser;
+    @Scheduled(fixedRate = 1000000)
     public void notifyUserWithNotSent() {
-        String user = "";
         String status = "unshipped";
-        sendOrderStatuses(user, status);
+        sendOrderStatuses(testUser, status);
     }
 
 }
