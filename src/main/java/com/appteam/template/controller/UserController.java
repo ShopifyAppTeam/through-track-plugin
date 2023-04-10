46d20ad3ba0190d65c4dc3b36060300329405aaa
Alina-Muha <AlinaMyxa2002@yandex.ru>
2023-04-10 17:58:03 +0300

    userController

--- src/main/java/com/appteam/template/controller/UserController.java
+++ src/main/java/com/appteam/template/controller/UserController.java
@@ -1,4 +1,42 @@
 package com.appteam.template.controller;
 
+import com.appteam.template.dto.UserData;
+import com.appteam.template.db.user.service.UserService;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+    @Resource(name = "userService")
+    private UserService userService;
+
+    @GetMapping
+    public List<UserData> allOrders() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping
+    public UserData addOrder(final @RequestBody UserData data) {
+        return userService.saveUser(data);
+    }
+
+    @GetMapping("/{id}")
+    public UserData getUser(final @PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public Boolean deleteOrder(final @PathVariable Long id) {
+        return userService.deleteUser(id);
+    }
 }
