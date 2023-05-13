bce013659917c230e35c8389efefd2b2ee1464d2
Alina-Muha <AlinaMyxa2002@yandex.ru>
2023-05-13 22:22:51 +0300

    fixed orderInfo controller

--- src/main/java/com/appteam/template/controller/OrderInfoController.java
+++ src/main/java/com/appteam/template/controller/OrderInfoController.java
@@ -1,33 +1,35 @@
 package com.appteam.template.controller;
 
+import com.appteam.template.dto.OrderData;
 import com.appteam.template.order_info.OrderInfo;
 import com.appteam.template.order_info.OrdersList;
 import com.shopify.ShopifySdk;
 import com.shopify.model.ShopifyShop;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+import java.util.ArrayList;
+
 @RestController
+@RequestMapping("/order-info")
 public class OrderInfoController {
 
-    @GetMapping("/orders-list")
-    public String OrdersList() {
-        try {
-            OrdersList ordersList = new OrdersList("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", "java-shop1");
-            return ordersList.getOrdersDataList().toString();
-        } catch (Exception exc) {
-            return exc.getMessage();
-        }
+    @GetMapping("/orders-list/{subdomain}/{token}")
+    public ResponseEntity<ArrayList<OrderData>> ordersList(final @PathVariable String token, final @PathVariable String subdomain) {
+        OrdersList ordersList = new OrdersList(token, subdomain);
+        return new ResponseEntity<>(ordersList.getOrdersDataList(), new HttpHeaders(), HttpStatus.CREATED);
     }
 
-    @GetMapping("/order-information")
-    public String orderInfo() {
-        try {
-            OrderInfo orderInfo= new OrderInfo("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", "java-shop1", "5282890514713");
-            return orderInfo.getOrderData().toString();
-        } catch (Exception exc) {
-            return exc.getMessage();
-        }
+    @GetMapping("/order-information/{subdomain}/{token}/{id}")
+    public ResponseEntity<OrderData> orderInfo(final @PathVariable String token, final @PathVariable String subdomain, final @PathVariable String id) {
+
+        OrderInfo orderInfo = new OrderInfo(token, subdomain, id);
+        return new ResponseEntity<>(orderInfo.getOrderData(), new HttpHeaders(), HttpStatus.CREATED);
     }
 }
